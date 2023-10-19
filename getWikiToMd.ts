5a2107d4386ce1f9ccb95d3678f13f2076a97ba9
Nikudanngo <satoru_rutosa@icloud.com>
2023-10-20 03:58:37 +0900

    delete jsonData/ and dataset/

--- getWikiToMd.ts
+++ getWikiToMd.ts
@@ -8,7 +8,7 @@ const api_url = "https://ja.wikipedia.org/w/api.php";
 const input = fs.readFileSync(path.resolve(__dirname, "./input.txt"), "utf-8") as string;
 
 // apiリクエスト
-const request = async (title: string) => {
+const Request = async (title: string) => {
   try {
     const params = {
       "action": "query",
@@ -23,78 +23,106 @@ const request = async (title: string) => {
     const content = pages[pageId].revisions[0]["*"] as string;
     return content;
   } catch (error) {
+    console.log(error);
     return "not found";
   }
 };
 
+// まず#REDIRECT or #転送を含むか確認
+// 含む場合true
+const IsRedirect = (content: string) => {
+  const match = content.match(/#REDIRECT|#転送/);
+  const isRedirect = match ? true : false;
+  return isRedirect;
+};
+
+// responseが#redirectの場合、redirect先のtitleを返す
+// #REDIRECT or #転送
+const GetRedirectTitle = (content: string) => {
+  const match = content.match(/#REDIRECT|#転送/);
+  const redirectTitle = match ? content.match(/\[\[(.*?)\]\]/)?.[1] : "not";
+  return redirectTitle as string;
+};
+
 // jsonとして保存
-const saveJson = async (title: string, content: string) => {
+const SaveJson = (title: string, content: string) => {
   fs.writeFileSync(path.resolve(__dirname, `./jsonData/${title}.json`), JSON.stringify(content));
 };
 
 // すでに./jsonData/にfetchしたjsonがあるか確認
-const isExistJson = async (title: string) => {
+const IsExistJsonData = (title: string) => {
   const files = fs.readdirSync(path.resolve(__dirname, "./jsonData/"));
   const isExist = files.includes(`${title}.json`);
   return isExist;
 };
 
 
-// ./wikiData/$title.txtに保存
-const saveMd = async (title: string, content: string) => {
-  fs.writeFileSync(path.resolve(__dirname, `./wikiData/${title}.md`), `# ${title} \n${content}`);
+// ./mdData/$title.txtに保存
+const SaveMd = (title: string, content: string) => {
+  fs.writeFileSync(path.resolve(__dirname, `./mdData/${title}.md`), `# ${title} \n${content}`);
 };
 
-// ./wikiData/allData.mdに追記して保存
-const appendSaveMd = async (title: string, content: string) => {
-  fs.appendFileSync(path.resolve(__dirname, `./wikiData/allData.md`), `# ${title} \n${content}\n`);
+// ./mdData/allData.mdに追記して保存
+const AppendSaveMd = (title: string, content: string) => {
+  fs.appendFileSync(path.resolve(__dirname, `./mdData/allData.md`), `# ${title} \n${content}\n`);
 };
 
 // .mdの中に# $titleがすでに保存されているか確認
-const isExistMd = async (title: string) => {
-  const allData = fs.readFileSync(path.resolve(__dirname, `./wikiData/allData.md`), "utf-8");
+const IsExistMdData = (title: string) => {
+  const allData = fs.readFileSync(path.resolve(__dirname, `./mdData/allData.md`), "utf-8");
   const isExist = allData.includes(`# ${title}`);
   return isExist;
 };
 
 // wikiTextの中身を整形
-const ParseWikiText = async (content: string) => {
-  // {{}}の中身を削除
+const ParseWikiText = (content: string) => {
+  // {{~}}を削除
   const deleteBrace = content.replace(/{{[\s\S]*?}}/g, "");
   // 参考文献以降を削除
-  const deleteReference = deleteBrace.replace(/(参考|脚注|参照)[\s\S]*/g, "");
+  const deleteReference = deleteBrace.replace(/(参考文献|脚注)[\s\S]*/g, "");
+  // <>,[[w:~]],[http~],[[(File:|画像)~thumb]]を削除
+  const deleteTag = deleteReference.replace(/<[\s\S]*?>|\[\[:?w:.*?\]\]|\[http.*?\]|\[(File:|画像:|ファイル:|Image:|file|image:).*?thumb/g, "");
+  // =,[,],',",{,} を削除
+  const deleteSymbol = deleteTag.replace(/=|'|"|\[|\]|\{|\}/g, "");
+  // #を*に変換
+  const convertSharp = deleteSymbol.replace(/#/g, "*");
   // 空白と\nを削除
-  const deleteBlank = deleteReference.replace(/\s|\\n/g, "");
-  // =, [] ,'," を削除
-  const deleteEqual = deleteBlank.replace(/=|'|"|\[|\]/g, "");
-  // <small/></small>の中を削除
-  const deleteSmall = deleteEqual.replace(/<small>[\s\S]*?<\/small>/g, "");
-  // <ref/>|</ref>,<refname"text">を削除
-  const deleteRef = deleteSmall.replace(/<ref>|<\/ref>|<refname[\s\S]*?>/g, "");
-  return deleteRef;
+  const deleteBlank = convertSharp.replace(/\s|\\n/g, "");
+  return deleteBlank;
 };
 
 const main = async () => {
   const titles = input.split("\n").map((line) => line.trim());
   for (const title of titles) {
     if (title === "") continue;
-    if (await isExistMd(title)) {
+    if (await IsExistMdData(title)) {
       console.log(`skip ${title}`);
       continue;
     }
-    if (await isExistJson(title)) {
+    if (await IsExistJsonData(title)) {
       const content = fs.readFileSync(path.resolve(__dirname, `./jsonData/${title}.json`), "utf-8");
       console.log(`${title} existed in ./jsonData/`);
-      const parseWikiText = await ParseWikiText(content);
-      appendSaveMd(title, parseWikiText);
+      const parseWikiText = ParseWikiText(content);
+      AppendSaveMd(title, parseWikiText);
       continue;
     }
-    console.log(`fetching ${title} and save to ./wikiData/${title}.md`);
+    console.log(`fetching ${title} and appending to ./mdData/allData.md`);
     await new Promise((resolve) => setTimeout(resolve, 2000));
-    const content = await request(title);
-    await saveJson(title, content);
-    const parseWikiText = await ParseWikiText(content);
-    appendSaveMd(title, parseWikiText);
+    const content = await Request(title);
+    // redirectの場合
+    if (IsRedirect(content)) {
+      const redirectTitle = GetRedirectTitle(content);
+      console.log(`redirect to ${redirectTitle}`);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const redirectContent = await Request(redirectTitle);
+      SaveJson(redirectTitle, redirectContent);
+      const parseWikiText = ParseWikiText(redirectContent);
+      AppendSaveMd(`${title}->${redirectTitle}`, parseWikiText);
+      continue;
+    }
+    SaveJson(title, content);
+    const parseWikiText = ParseWikiText(content);
+    AppendSaveMd(title, parseWikiText);
   }
 };
 
--- makeDataset.ts
+++ makeDataset.ts
@@ -1,13 +1,3 @@
-// input:{
-// # ドクガ
-// - 分類: チョウ目ドクガ科
-// - 学名: Artaxa subflava
-// - 特徴: 鱗翅目の仲間 幼虫が持っている毒針毛が皮膚炎を起こす 幼虫は5月～6月頃に見られ、体色は黒く、背や側面にオレンジ色の班紋がある 成虫は6月～8月頃に現れ、黄褐色で20～30ミリメートル程度の大きさになる 初期の幼虫は体長1センチ未満で終齢幼虫になると約3.5 - 4.0センチ サクラ・ウメ・ツツジのほか、多くの植物を食害する ドクガの幼虫が持っている毒針毛に触れると、かゆみをともなって赤く腫れ、1日～2日後にかゆみの強い赤い発疹ができます。かゆみは2～3週間続きます。症状のあらわれ方や程度には個人差があります。 ドクガの幼虫は成熟すると分散するのに対して、チャドクガの毛虫は全生育期間を通じて、集団生活を続けるという習性の違いもあります
-// - 毒性:
-// - 可食性:
-// }
-// output:{ "messages": [{ "role": "user", "content": "inputA" }, { "role": "assistant", "content": "inputB" }]; }
-// この形式のデータセットを生成したい
 import fs from "fs";
 import path from "path";
 import JSONL from "jsonl-parse-stringify";
@@ -24,7 +14,7 @@ interface ParsedMd {
   content: string;
 }
 
-const input = fs.readFileSync(path.resolve(__dirname, "./input.md"), "utf-8");
+const input = fs.readFileSync(path.resolve(__dirname, "./mdData/allData.md"), "utf-8");
 const linesText = input.split("\#").filter((line) => line !== "");
 
 const parsedMd: ParsedMd[] = linesText.map((line) => {
@@ -36,7 +26,7 @@ const wikiToDataset = (parsedMd: ParsedMd[]): Dataset[] => {
   const datasets = parsedMd.map((line) => {
     const messages: Message[] = [
       { role: "user", content: line.title },
-      { role: "assistant", content: line.content },
+      { role: "assistant", content: line.content.replace(/\n/g, "") },
     ];
     console.log(messages);
     return { messages };
@@ -45,8 +35,8 @@ const wikiToDataset = (parsedMd: ParsedMd[]): Dataset[] => {
 };
 
 const saveDataset = (dataset: Dataset[]) => {
-  fs.writeFileSync(path.resolve(__dirname, "./jsonData/dataset.json"), JSON.stringify(dataset, null, 2));
-  fs.writeFileSync(path.resolve(__dirname, "./jsonData/dataset.jsonl"), JSONL.stringify(dataset));
+  fs.writeFileSync(path.resolve(__dirname, "./dataset/dataset.json"), JSON.stringify(dataset, null, 2));
+  fs.writeFileSync(path.resolve(__dirname, "./dataset/dataset.jsonl"), JSONL.stringify(dataset));
 };
 
 const dataset = wikiToDataset(parsedMd);

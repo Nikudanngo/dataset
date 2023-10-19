15e47f99a63f288e13592fdaaaeabb7bc86e445b
Nikudanngo <satoru_rutosa@icloud.com>
2023-10-19 23:37:24 +0900

    json バックアップ追加

--- getWikiToMd.ts
+++ getWikiToMd.ts
@@ -5,7 +5,7 @@ const path = require("path");
 const api_url = "https://ja.wikipedia.org/w/api.php";
 
 // apiリクエスト
-const request = async (title) => {
+const request = async (title: string) => {
   const params = {
     "action": "query",
     "format": "json",
@@ -20,13 +20,26 @@ const request = async (title) => {
   return content;
 };
 
+// jsonとして保存
+const saveJson = async (title: string, content: string) => {
+  fs.writeFileSync(path.resolve(__dirname, `./jsonData/${title}.json`), JSON.stringify(content));
+};
+
+// すでに./jsonData/にfetchしたjsonがあるか確認
+const isExistJson = async (title: string) => {
+  const files = fs.readdirSync(path.resolve(__dirname, "./jsonData/"));
+  const isExist = files.includes(`${title}.json`);
+  return isExist;
+};
+
+
 // ./wikiData/$title.txtに保存
-const saveMd = async (title, content) => {
+const saveMd = async (title: string, content: string) => {
   fs.writeFileSync(path.resolve(__dirname, `./wikiData/${title}.md`), `# ${title} \n${content}`);
 };
 
 // ./wikiData/allData.mdに追記して保存
-const appendSaveMd = async (title, content) => {
+const appendSaveMd = async (title: string, content: string) => {
   fs.appendFileSync(path.resolve(__dirname, `./wikiData/allData.md`), `# ${title} \n${content}\n`);
 };
 
@@ -38,28 +51,40 @@ const isExistMd = async (title: string) => {
 };
 
 // wikiTextの中身を整形
-const ParseWikiText = async (content) => {
+const ParseWikiText = async (content: string) => {
   // {{}}の中身を削除
   const deleteBrace = content.replace(/{{[\s\S]*?}}/g, "");
   // 参考文献以降を削除
-  const deleteReference = deleteBrace.replace(/参考文献[\s\S]*/, "");
-  // 空白を削除
-  const deleteBlank = deleteReference.replace(/\s+/g, "");
-  // =, [] ,' を削除
-  const deleteEqual = deleteBlank.replace(/=|'|\[|\]/g, "");
-  return deleteEqual;
+  const deleteReference = deleteBrace.replace(/(参考|脚注|参照)[\s\S]*/g, "");
+  // 空白と\nを削除
+  const deleteBlank = deleteReference.replace(/\s|\\n/g, "");
+  // =, [] ,'," を削除
+  const deleteEqual = deleteBlank.replace(/=|'|"|\[|\]/g, "");
+  // <small/></small>の中を削除
+  const deleteSmall = deleteEqual.replace(/<small>[\s\S]*?<\/small>/g, "");
+  // <ref/>|</ref>,<refname"text">を削除
+  const deleteRef = deleteSmall.replace(/<ref>|<\/ref>|<refname[\s\S]*?>/g, "");
+  return deleteRef;
 };
 
 const main = async () => {
-  const titles = ["ドクガ", "タランチュラ"];
+  const titles = ["ドクガ", "タランチュラ", "チャドクガ"];
   for (const title of titles) {
-    console.log(title);
     if (await isExistMd(title)) {
-      console.log("skip");
+      console.log(`skip ${title}`);
       continue;
     }
+    if (await isExistJson(title)) {
+      const content = fs.readFileSync(path.resolve(__dirname, `./jsonData/${title}.json`), "utf-8");
+      console.log(`${title} existed in ./jsonData/`);
+      const parseWikiText = await ParseWikiText(content);
+      appendSaveMd(title, parseWikiText);
+      continue;
+    }
+    console.log(`fetching ${title} and save to ./wikiData/${title}.md`);
     await new Promise((resolve) => setTimeout(resolve, 2000));
     const content = await request(title);
+    await saveJson(title, content);
     const parseWikiText = await ParseWikiText(content);
     appendSaveMd(title, parseWikiText);
   }
